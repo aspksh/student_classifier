9377f98e0368b70c0326faca4d225f29ed2f0190
aspksh <aspksh@gmail.com>
2026-06-23 17:33:46 +0530

    Update student_classifier.py

--- student_classifier.py
+++ student_classifier.py
@@ -1,20 +1,15 @@
-import torch.nn as nn
-
 class StudentClassifier(nn.Module):
-
     def __init__(self):
-
         super().__init__()
-
-        self.layer1 = nn.Linear(2,4)
-        self.relu = nn.ReLU()
-        self.layer2 = nn.Linear(4,1)
-
-    def forward(self,x):
-
+        self.layer1 = nn.Linear(2, 16)
+        self.relu1  = nn.ReLU()
+        self.layer2 = nn.Linear(16, 8)
+        self.relu2  = nn.ReLU()
+        self.layer3 = nn.Linear(8, 1)
+    def forward(self, x):
         x = self.layer1(x)
-        x = self.relu(x)
+        x = self.relu1(x)
         x = self.layer2(x)
-
+        x = self.relu2(x)
+        x = self.layer3(x)
         return x
-
